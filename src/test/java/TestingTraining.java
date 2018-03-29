ff056dcd001ab3c9f5b33b6e5fef7cbd9c57fa29
Zhenya2210 <savzh@yandex.ru>
2018-03-29 17:26:51 +0300

    Initial commit

---
+++ src/test/java/TestingTraining.java
@@ -0,0 +1,47 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class TestingTraining {
+
+    @BeforeAll
+    public void setUP(){
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+
+
+    }
+
+    @Test
+    public void checkGetRequest(){
+            given()
+            .accept(ContentType.JSON)
+            .when()
+                    .get("/posts/2")
+            .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK);
+
+            String s = given()
+                    .accept(ContentType.JSON)
+                    .when()
+                        .get("/posts/2")
+                    .thenReturn()
+                        .asString();
+
+        JsonPath json = new JsonPath(s);
+        int userId = json.getInt("userId");
+        assertEquals(1, userId);
+
+
+
+    }
+
+}
