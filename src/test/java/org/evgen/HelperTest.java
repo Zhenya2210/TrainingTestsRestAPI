9496e13bbc5701721997cc0e725be2343be651bf
Zhenya2210 <savzh@yandex.ru>
2018-04-06 17:49:44 +0300

    Changes

--- src/test/java/org/evgen/HelperTest.java
+++ src/test/java/org/evgen/HelperTest.java
@@ -2,11 +2,13 @@ package org.evgen;
 
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
+import org.evgen.dogs.testing.Dog;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -71,4 +73,25 @@ public class HelperTest {
         }
         return valueOfMessage;
     }
+
+    public static List<Dog> getCorrectDogs(){
+        final String nameOneHundredCharacters = "Представьте себе офисную ATC, которую можно подключить через Интернет в течение нескольких минут, бе";
+        final String nameOneHundredMinusOneCharacters = "Представьте себе офисную ATC, которую можно подключить через Интернет в течение нескольких минут, б";
+
+        List<Dog> dogs = new ArrayList<>();
+            Dog dog1 = new Dog("I", 13.0, 50.0);
+            Dog dog2 = new Dog("Dino", 50.3, 10.7);
+            Dog dog3 = new Dog("Rex", 13.121239401294853, 50.34234);
+            Dog dog4 = new Dog(nameOneHundredCharacters, 13.121, 50.34234);
+            Dog dog5 = new Dog(nameOneHundredMinusOneCharacters, 20.1, 14.5);
+            Dog dog6 = new Dog("Bobik", 1.7e+308, 1.7e+308);
+        dogs.add(dog1);
+        dogs.add(dog2);
+        dogs.add(dog3);
+        dogs.add(dog4);
+        dogs.add(dog5);
+        dogs.add(dog6);
+        return dogs;
+    }
+
 }
--- src/test/java/org/evgen/dogs/testing/TestingDogsWithoutDateOfBirth.java
+++ src/test/java/org/evgen/dogs/testing/TestingDogsWithoutDateOfBirth.java
@@ -1,12 +1,12 @@
 package org.evgen.dogs.testing;
 
 import io.restassured.RestAssured;
+import org.evgen.HelperTest;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
@@ -16,9 +16,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * Testing application github: https://github.com/timazet/java-course
  */
 public class TestingDogsWithoutDateOfBirth {
-
-    final static String nameOneHundredCharacters = "Представьте себе офисную АТС, которую можно подключить через Интернет в течение нескольких минут, бе";
-
 
 
     @BeforeAll
@@ -77,16 +74,7 @@ public class TestingDogsWithoutDateOfBirth {
     }
 
     public static List<Dog> valuesForMethodCreateDogs() {
-        List<Dog> dogs = new ArrayList<>();
-            Dog dog1 = new Dog("Kia", 13.0, 50.0);
-            Dog dog2 = new Dog("Koko", 50.3, 10.7);
-            Dog dog3 = new Dog("Loko", 13.121, 50.34234);
-            Dog dog4 = new Dog(nameOneHundredCharacters, 13.121, 50.34234);
-        dogs.add(dog1);
-        dogs.add(dog2);
-        dogs.add(dog3);
-        dogs.add(dog4);
 
-        return dogs;
+        return HelperTest.getCorrectDogs();
     }
 }
